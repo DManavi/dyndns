648a60f70165b28c83cbecf2d958b35d8da76a4e
Danial Manavi <dmanavi@live.com>
2023-10-26 21:59:54 +0000

    fix(shared): converted function definitions to interfaces

--- packages/shared/src/types/ip_retriever.ts
+++
@@ -1,8 +0,0 @@
-// package-level
-import { IPFamily } from './shared';
-
-/**
- * Retrieve public IP address of the requested family
- * @param {IPFamily} family IP address family
- */
-export type IPRetriever = (family: IPFamily) => Promise<string>;
